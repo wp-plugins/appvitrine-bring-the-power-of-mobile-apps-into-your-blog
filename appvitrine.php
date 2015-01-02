679f65533268145dfc422b37e173b09c4a90556b
Lookitap <Lookitap@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-01-02 20:41:22 +0000

    Edit Description
    git-svn-id: https://plugins.svn.wordpress.org/appvitrine-bring-the-power-of-mobile-apps-into-your-blog/tags/1.1@1058647 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- appvitrine.php
+++ appvitrine.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: AppVitrine
 Plugin URI: http://lookitap.com/appvitrine.php
-Description: An app-recommendation plugin which enriches your blog by automatically placing the best related apps to your content into your blog post.
+Description: An app-recommendation plugin which enriches your blog by automatically placing the best related apps to your content into your blog post
 Version: 1.1
 Author: Lookitap
 Author URI: http://lookitap.com
