1322c458ef3a944405f24e331adfc86fc74217e9
Lookitap <Lookitap@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-08-06 02:46:54 +0000

    Delete line break
    git-svn-id: https://plugins.svn.wordpress.org/appvitrine-bring-the-power-of-mobile-apps-into-your-blog/tags/2.3@1213829 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- appvitrine.php
+++ appvitrine.php
@@ -8,7 +8,6 @@
  License: GPLv2
  Usage: [appvitrine size="normal" keywords="keyword1,keyword2,keyword3"]
  */
-
 /********************************************************************************/
 // set default parameters
 if (get_option("appvitrine_plugin_initiated") != "true") {
